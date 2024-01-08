655ad630797eb6e768bdc8afc93804f5659ef841
Ádám Timur Yilmaz <adam.y@saltmine.com>
2024-01-08 13:20:55 +0100

    fix:clean branch

--- srcPort/Common/Shaders/Vert.ts
+++
@@ -1,9 +0,0 @@
-export default `
-varying vec3 vUv; 
-
-void main() {
-	vUv = position; 
-
-	vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-	gl_Position = projectionMatrix * modelViewPosition; 
-}`;
