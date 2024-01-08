2d0aaef1338a47876f3e14f2833d0ade60f69205
Ádám Timur Yilmaz <adam.y@saltmine.com>
2024-01-08 13:37:38 +0100

    feat:basic viewer file and threejs scene

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("asd")
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+
+function init(): void {
+    // init threejs scene
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+window.onload = init;
